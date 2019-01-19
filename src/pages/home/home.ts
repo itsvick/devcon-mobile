a21e3367737ffac0d7965ff2d6b5f55b821fcb46
diptesh6501 <diptesh1993@gmail.com>
2019-01-19 18:48:19 +0530

    list mock up added in view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,38 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  //sampleData
 
   constructor(public navCtrl: NavController) {
 
   }
+
+  datas = [
+    {
+     title:'content1',
+     thumbnail : 'https://www.amplifymm.com/wp-content/uploads/2013/12/GTD-for-Content-Marketing-01.jpeg',
+     linkUrl : 'link_1'
+    },{
+      title:'content1',
+      thumbnail : 'https://www.amplifymm.com/wp-content/uploads/2013/12/GTD-for-Content-Marketing-01.jpeg',
+      linkUrl : 'link_1'
+     },{
+      title:'content2',
+      thumbnail : 'https://www.amplifymm.com/wp-content/uploads/2013/12/GTD-for-Content-Marketing-01.jpeg',
+      linkUrl : 'link_1'
+     },{
+      title:'content3',
+      thumbnail : 'https://www.amplifymm.com/wp-content/uploads/2013/12/GTD-for-Content-Marketing-01.jpeg',
+      linkUrl : 'link_1'
+     },{
+      title:'content4',
+      thumbnail : 'https://www.amplifymm.com/wp-content/uploads/2013/12/GTD-for-Content-Marketing-01.jpeg',
+      linkUrl : 'link_1'
+     },{
+      title:'content5',
+      thumbnail : 'https://www.amplifymm.com/wp-content/uploads/2013/12/GTD-for-Content-Marketing-01.jpeg',
+      linkUrl : 'link_1'
+     }
+  ]
 
 }
